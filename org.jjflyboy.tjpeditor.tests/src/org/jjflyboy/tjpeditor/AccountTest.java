8a77af0de45fe8379923b0e90f3f57a286403a6b
John J. Franey <jjfraney@gmail.com>
2011-10-10 21:57:00 -0400

    provides Account unit test

---
+++ org.jjflyboy.tjpeditor.tests/src/org/jjflyboy/tjpeditor/AccountTest.java
@@ -0,0 +1,47 @@
+package org.jjflyboy.tjpeditor;
+
+import org.eclipse.xtext.junit4.InjectWith;
+import org.eclipse.xtext.junit4.XtextRunner;
+import org.eclipselabs.xtext.utils.unittesting.XtextTest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@InjectWith(ProjectInjectorProvider.class)
+@RunWith(XtextRunner.class)
+public class AccountTest  extends XtextTest {
+	
+	
+	@Test 
+	public void test() {
+		testParserRule("account \"test account\"", "Account");
+	}
+	
+	@Test 
+	public void testWithId() {
+		testParserRule("account acso \"test account\"", "Account");
+	}
+	
+	
+	@Test 
+	public void testEmptyAttributes() {
+		testParserRule("account \"test account\" { } ", "Account");
+	}
+	
+	@Test
+	public void testWithSubAccount() {
+		testParserRule("account \"Test account\" { account \"sub account\" }", "Account");
+	}
+	
+	@Test 
+	public void testWithCredit() {
+		testParserRule("account \"test account\" { credit 2011-12-11 \"extra credit\" 4.30 } ", "Account");
+	}
+	
+	@Test 
+	public void testWithFlags() {
+		testParserRule("account \"test account\" { credit 2011-12-11 \"extra credit\" 4.30 flags flg1, flg2, flg3} ", "Account");
+	}
+	
+
+
+}
